32eed2fa9c15fdd4e97c843d7429686dbe31d2d6
BlondelMbou <blondel.mbou@gmail.com>
2024-11-26 00:14:58 +0100

    formatage du code

--- tests/inventoryTest.spec.ts
+++ tests/inventoryTest.spec.ts
@@ -4,18 +4,16 @@ import { InventoryPage } from '../pages/InventoryPage';
 import { LoginPage } from '../pages/LoginPage';
 
 test.describe('Inventory Tests', () => {
-  test('Add an item to the cart', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigate();
-    await loginPage.login('standard_user', 'secret_sauce');
+    test('Add an item to the cart', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await loginPage.login('standard_user', 'secret_sauce');
+        const inventoryPage = new InventoryPage(page);
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+        await inventoryPage.addItemToCart(0);
+        const cartCount = await inventoryPage.getCartItemCount();
+        expect(cartCount).toBe(1);
+    });
 
-    const inventoryPage = new InventoryPage(page);
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-    await inventoryPage.addItemToCart(0); 
-    const cartCount = await inventoryPage.getCartItemCount();
-    expect(cartCount).toBe(1);
-  });
-
-  
 });
