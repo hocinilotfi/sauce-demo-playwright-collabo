d588666d7fc80c044559adbb418d97269f06a6af
BlondelMbou <blondel.mbou@gmail.com>
2024-11-25 23:59:14 +0100

    implementation du iventorypage && inventorytest

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,34 @@
+import { Page, Locator } from '@playwright/test';
+
+export class InventoryPage {
+  private page: Page;
+
+  private inventoryItems = '.inventory_item';
+  private addToCartButton = '.btn_inventory';
+  private cartBadge = '.shopping_cart_badge';
+  private itemName = '.inventory_item_name';
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async addItemToCart(index: number) {
+    const items = await this.page.$$(this.inventoryItems);  
+    const addToCartButton = await items[index].$(this.addToCartButton);  
+  
+    if (addToCartButton) {  
+      await addToCartButton.click();  
+    } else {
+      throw new Error("erro");
+    }
+  }
+  
+
+  async getCartItemCount(): Promise<number> {
+    const badge = await this.page.locator(this.cartBadge);
+    const count = await badge.textContent();
+    return count ? parseInt(count) : 0;
+  }
+
+  
+}
